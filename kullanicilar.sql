564ec701b0e3c9b5e3a8089a5177275889e7fe5c
Mr-Simsek <aliosmansimsek713@gmail.com>
2024-06-17 21:51:51 +0300

    Add files via upload

---
+++ kullanicilar.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 20 May 2024, 21:30:55
+-- Sunucu sürümü: 10.4.32-MariaDB
+-- PHP Sürümü: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `kirtasiye`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanicilar`
+--
+
+CREATE TABLE `kullanicilar` (
+  `kullanici` varchar(30) NOT NULL,
+  `sifre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Tablo döküm verisi `kullanicilar`
+--
+
+INSERT INTO `kullanicilar` (`kullanici`, `sifre`) VALUES
+('admin', '1234');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kullanicilar`
+--
+ALTER TABLE `kullanicilar`
+  ADD PRIMARY KEY (`kullanici`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * WordPress için başlangıç ayar dosyası.
+ *
+ * Bu dosya kurulum sırasında wp-config.php dosyasının oluşturulabilmesi için
+ * kullanılır. İsterseniz bu dosyayı kopyalayıp, ismini "wp-config.php" olarak değiştirip,
+ * değerleri girerek de kullanabilirsiniz.
+ *
+ * Bu dosya şu ayarları içerir:
+ * 
+ * * Veritabanı ayarları
+ * * Gizli anahtarlar
+ * * Veritabanı tablo ön eki
+ * * ABSPATH
+ * 
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Veritabanı ayarları - Bu bilgileri servis sağlayıcınızdan alabilirsiniz ** //
+/** WordPress için kullanılacak veritabanının adı */
+define( 'DB_NAME', 'wepsitesii' );
+
+/** Veritabanı kullanıcısı */
+define( 'DB_USER', 'aliiosman' );
+
+/** Veritabanı parolası */
+define( 'DB_PASSWORD', '' );
+
+/** Veritabanı sunucusu */
+define( 'DB_HOST', 'localhost' );
+
+/** Yaratılacak tablolar için veritabanı karakter seti. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Veritabanı karşılaştırma tipi. Herhangi bir şüpheniz varsa bu değeri değiştirmeyin. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Eşsiz doğrulama anahtarları ve tuzlar.
+ *
+ * Her anahtar farklı bir karakter kümesi olmalı!
+ * {@link http://api.wordpress.org/secret-key/1.1/salt WordPress.org secret-key service} servisini kullanarak yaratabilirsiniz.
+ * 
+ * Çerezleri geçersiz kılmak için istediğiniz zaman bu değerleri değiştirebilirsiniz.
+ * Bu tüm kullanıcıların tekrar giriş yapmasını gerektirecektir.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '7dqs[mA)9?w@*Ng 7>(@Mf;].bk8bc6zr2]K*q^}iu-ddqb$l:R8ruQ0!:D:i[]N' );
+define( 'SECURE_AUTH_KEY',  'iL9I4a -U(Byi#c:$jN[Wvbnd`zlJx(+rxixVoKz=2Kula7 @QN7OL%9+mHa2yjm' );
+define( 'LOGGED_IN_KEY',    'QQ=&fmcqejJEO=6I.%|r;8%x@aL#=7HBj|*tRVpQ>7IRti )0.B:Xl]^Biga7Zx=' );
+define( 'NONCE_KEY',        '<).;t,[F>jd4N&Ow@F$Q04~5T6X*x+[!K/VD(m/XW?gQ;i|QSn!~i2qMKswCB403' );
+define( 'AUTH_SALT',        'XE<k~|DQ+/ r_|<Fzh4tb_b9v=xX/ez8wHP[#T/y^g-lY{Q}mqG$BH[UHG)Q5=_E' );
+define( 'SECURE_AUTH_SALT', 'K/L)#[h4)9FR%aV*uYK*;Bc krT0]@;#Dqx/rp/6JZm m7)IwDPX<_ZZH/w)OnYG' );
+define( 'LOGGED_IN_SALT',   'BD:Bdq@SK_JsZt9X`=&I5h/GP*M1THu4t4ng=N:((Sx,z{ikFxacuLENAo0!G%.i' );
+define( 'NONCE_SALT',       'FfLT5{UF]9sU{g$`_FUcB#0_LwujLsQ{_G~^WN,d(m_}2M8Y>%%.P6-MIds4!GeF' );
+
+/**#@-*/
+
+/**
+ * WordPress veritabanı tablo ön eki.
+ *
+ * Tüm kurulumlara ayrı bir önek vererek bir veritabanına birden fazla kurulum yapabilirsiniz.
+ * Sadece rakamlar, harfler ve alt çizgi lütfen.
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Geliştiriciler için: WordPress hata ayıklama modu.
+ *
+ * Bu değeri true olarak ayarlayıp geliştirme sırasında hataların ekrana
+ * basılmasını sağlayabilirsiniz. Tema ve eklenti geliştiricilerinin geliştirme
+ * aşamasında WP_DEBUG kullanmalarını önemle tavsiye ederiz.
+ * 
+ * Hata ayıklama için kullanabilecek diğer sabitler ile ilgili daha fazla bilgiyi
+ * belgelerden edinebilirsiniz.
+ * 
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Her türlü özel değeri bu satı ile "Hepsi bu kadar" yazan satır arasına ekleyebilirsiniz. */
+
+
+
+/* Hepsi bu kadar. Mutlu bloglamalar! */
+
+/** WordPress dizini için mutlak yol. */
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** WordPress değişkenlerini ve yollarını kurar. */
+require_once ABSPATH . 'wp-settings.php';
